7e952cee7cc897be3159299169d14fae36227aee
Md Minhaj <parsonal322532@gmail.com>
2024-02-08 23:30:08 +0600

    Express MongoDB Cors Dotenv Added

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.POST || 3000;
+
+// MiddleWare
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xevudqv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+
+    // MongoDB Collection
+    const database = client.db("summerCourseSchoolDB");
+    const users = database.collection("users");
+
+    // Test API
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    // Users API
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port);
